2fa9004c8db6e9e81fb86fe0309c5d4c0f474b53
kh-0626 <kdw052516@naver.com>
2025-01-20 11:25:27 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,21 @@
+// JS와 주석 동일
+
+/* Run 버튼 (단축키 ctr1 + f11)
+ * 1) 자동으로 컴파일 수행(javac ooo.java) 
+ * -> .class 파일(byte code) 생성 
+ * 
+ * 2) 컴파일 완료 후 .class 파일 실행 (java class파일명)
+ */
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이클립스 실행 중");
+		System.out.println("자꾸 그러지마....");
+		System.out.println("타자 연습좀 해!!!!!");
+		System.out.println("으잉!");
+
+	}
+
+}
