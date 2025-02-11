dc3ca0755671023fbcf995a7958b9ec057fbc10e
kh-0626 <kdw052516@naver.com>
2025-02-11 09:53:47 +0900

    20250210

---
+++ 10_generics_collection/src/set/service/SetRun.java
@@ -0,0 +1,13 @@
+package set.service;
+
+public class SetRun {
+	public static void main(String[] args) {
+		
+		SetService sercvice = new SetService();
+		
+//		sercvice.test4();
+//		sercvice.getLottoNumber();
+		sercvice.lottoNumberGenrator();
+		
+	}
+}
